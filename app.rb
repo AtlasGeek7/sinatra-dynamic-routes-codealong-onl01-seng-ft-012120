7d6fc929969b82462f59a76e79ac5bb38cf42d9d
Khalid T <khalid.tissire@gmail.com>
2020-03-07 14:08:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
   
     get "/multiply/:num1&:num2" do
-    num1 = params[:num1]
-    "Goodbye, #{@user_name}!"
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    "#{num1}!"
   end
 
 end
